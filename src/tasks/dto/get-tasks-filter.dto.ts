0f30c929b980c9db7ccd7968a2bfff9b1afb078d
Elves Brito <elvesbd@yahoo.com>
2021-09-09 16:13:24 -0300

    chore: use global pipes

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.interface';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
